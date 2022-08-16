9ebeffd336480eff79a4caa67382da172ec47394
pyo1748 <pyo1748@estsecurity.com>
2022-08-16 11:11:42 +0900

    feat: auth decorator 구현

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 
+import { AuthUser } from '~src/modules/auth/auth-user.decorator';
 import { AuthGuard } from '~src/modules/auth/auth.guard';
+import { User } from '~src/user/user.entity';
 
 @Controller('test')
 export class TestController {
   @UseGuards(AuthGuard)
   @Get()
-  test() {
+  test(@AuthUser() authUser: User) {
+    console.log(authUser);
     return 'Hello World';
   }
 }
